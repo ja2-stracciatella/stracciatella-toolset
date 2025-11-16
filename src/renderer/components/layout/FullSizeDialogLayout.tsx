01dc38b0f1850b19b9c894dc6cd0d21c2b86153b
Stefan Lau <github@stefanlau.com>
2025-11-16 15:05:32 +0100

    Refactor FullSizeDialogLayout

--- src/renderer/components/layout/FullSizeDialogLayout.tsx
+++ src/renderer/components/layout/FullSizeDialogLayout.tsx
@@ -1,14 +1,45 @@
-import { Layout } from 'antd';
-import './FullSizeDialogLayout.css';
+import { Flex, theme } from 'antd';
+import { useMemo } from 'react';
+
+const FLEX_BASE_STYLE = {
+  height: '100vh',
+  width: '100vw',
+  overflowX: 'hidden',
+  overflowY: 'hidden',
+} as const;
+
+const DIALOG_STYLE = {
+  position: 'relative',
+  background: 'white',
+  width: 'calc(100% - 20px)',
+  maxWidth: '1024px',
+  maxHeight: 'calc(100% - 20px)',
+  minHeight: '60vh',
+  margin: '10px',
+  padding: '20px',
+  overflow: 'auto',
+  flexGrow: 1,
+} as const;
 
 export function FullSizeDialogLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const {
+    token: { colorBgLayout },
+  } = theme.useToken();
+  const FLEX_STYLE = useMemo(
+    () => ({
+      ...FLEX_BASE_STYLE,
+      backgroundColor: colorBgLayout,
+    }),
+    [colorBgLayout],
+  );
+
   return (
-    <Layout className="full-size-dialog-layout-root">
-      <div className="full-size-dialog-layout-content">{children}</div>
-    </Layout>
+    <Flex vertical align="center" justify="center" style={FLEX_STYLE}>
+      <div style={DIALOG_STYLE}>{children}</div>
+    </Flex>
   );
 }
